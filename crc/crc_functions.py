6e214b39517693f6f109b56234f14bfce568e79d
cjsalinas <47445339+xkaze09@users.noreply.github.com>
2024-11-24 01:05:07 +0800

    refactor: improve string to binary conversion and enhance function documentation

--- crc/crc_functions.py
+++ crc/crc_functions.py
@@ -9,12 +9,15 @@ changes/errors in the communication channel.
 import random
 
 def string_to_binary(input_string):
-    data = (''.join(format(ord(x), 'b') for x in input_string))
-    return data
+    '''
+    Convert a string to binary representation 
+    '''
+    return ''.join(format(ord(char), '08b') for char in input_string)
     
 
 def mod_2_division(dividend, divisor):
-    ''' Perform modulo 2 division and return the remainder
+    '''
+    Perform modulo 2 division and return the remainder
     Args:
     - dividend: message with appended 0s
     - divisor: binary string of generator
@@ -36,32 +39,28 @@ def mod_2_division(dividend, divisor):
     # Return the remainder, used as the CRC checksum
     return ''.join(dividend[-(len_divisor -1):])  # last n-1 bits of the dividend
 
-def crc(data, generator): 
+def crc(data, generator):
     '''
-    Compute the CRC Checksum for the message using a generator polynomial G(X)
+    Compute the CRC Checksum 
     '''
-    padded_data = data + '0' * (len(generator)-1) # Append n 0s to M(X)
+    padded_data = data + '0' * (len(generator) - 1)  # Append n zeros
     remainder = mod_2_division(padded_data, generator)
     return remainder
 
 def validate_crc(received_msg, generator):
     '''
-    Validate CRC of a received message
+    Validate CRC of a received message 
     '''
-
-    # Split the message and checksum
     data, received_crc = received_msg[:-len(generator)+1], received_msg[-len(generator)+1:]
-    # Recompute CRC for the received data
     computed_crc = crc(data, generator)
     return received_crc == computed_crc
 
 def introduce_error(msg, error_chance=5):
     '''
-    Introduce a 5% chance of adding 1 bit to T(X)
+    Introduce a 5% chance of adding 1 bit to T(X) 
     '''
-
-    if random.randint(1,100) <= error_chance:
-        bit_to_flip = random.randint(0, len(msg)-1)
+    if random.randint(1, 100) <= error_chance:
+        bit_to_flip = random.randint(0, len(msg) - 1)
         flipped_bit = '1' if msg[bit_to_flip] == '0' else '0'
         msg = msg[:bit_to_flip] + flipped_bit + msg[bit_to_flip + 1:]
         print(f"Error introduced at bit {bit_to_flip}.")
